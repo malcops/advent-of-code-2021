5cfb34381778d5e8e4db58088253ebeda140e191
malcops <paul.malcolm@protonmail.com>
2021-12-03 12:27:57 -0500

    fixup! [UPDATE] generate.py

--- generate.py
+++ generate.py
@@ -14,15 +14,15 @@ with open(f"{day}/Test{day}.cpp", 'w') as f:
     f.write(f'''#include "{day}.hpp"\n''')
 
     f.write('''#include "gtest/gtest.h"\n\n''')
-    f.write("TEST(Test{}, initialize)\n".format(day))
-    f.write("{")
+    f.write("TEST(Test{}, initialize)".format(day))
+    f.write('''{\n''')
     f.write('''    ASSERT_EQ(1, 1);\n''')
-    f.write('''    }\n''')
+    f.write('''    }\n\n''')
 
-    f.write('''    int main(int argc, char **argv) {\n''')
-    f.write('''      ::testing::InitGoogleTest(&argc, argv);\n''')
-    f.write('''    return RUN_ALL_TESTS();\n''')
-    f.write('''    }\n''')
+    f.write('''int main(int argc, char **argv) {\n''')
+    f.write('''  ::testing::InitGoogleTest(&argc, argv);\n''')
+    f.write('''return RUN_ALL_TESTS();\n''')
+    f.write('''}\n''')
 
 
 
