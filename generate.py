07919f1e371ea76c053cb5d137140ed9e6a6af59
malcops <paul.malcolm@protonmail.com>
2021-12-06 14:34:13 -0500

    fixup! fixup! [UPDATE] generate.py

--- generate.py
+++ generate.py
@@ -17,7 +17,7 @@ with open(f"{day}/Test{day}.cpp", 'w') as f:
     f.write("TEST(Test{}, initialize)".format(day))
     f.write('''{\n''')
     f.write('''    ASSERT_EQ(1, 1);\n''')
-    f.write('''    }\n\n''')
+    f.write('''}\n\n''')
 
     f.write('''int main(int argc, char **argv) {\n''')
     f.write('''  ::testing::InitGoogleTest(&argc, argv);\n''')
