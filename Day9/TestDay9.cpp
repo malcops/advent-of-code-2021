0dcedf403aa6aa139e93696701f537783304f09c
malcops <paul.malcolm@protonmail.com>
2021-12-22 14:19:22 -0500

    fixup! day9 brute force with 10-padding

--- Day9/TestDay9.cpp
+++ Day9/TestDay9.cpp
@@ -6,6 +6,11 @@ TEST(TestDay9, part1example){
     ASSERT_EQ(part1(in), 15);
 }
 
+TEST(TestDay9, part1){
+    auto in= parse_input("Day9/input.txt");
+    ASSERT_EQ(part1(in), 518);
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
 return RUN_ALL_TESTS();
